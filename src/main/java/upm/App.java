54f3de5ab57554156903354a8fcd4e5dedd3edd7
unknown <matias.rodriguez@alumnos.upm.es>
2024-09-16 17:55:41 +0200

    Initial

---
+++ src/main/java/upm/App.java
@@ -0,0 +1,9 @@
+package upm;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Run App..");
+
+    }
+}
---
+++ src/main/java/upm/Usuario.java
@@ -0,0 +1,37 @@
+package upm;
+
+public class Usuario {
+    private String nombre;
+    private String fechaNacimiento;
+    private char genero;
+    private String correo;
+    private String contra;
+    public Usuario(String nombre, String fechaNacimiento, char genero, String correo, String contra) {
+        this.nombre=nombre;
+        this.fechaNacimiento=fechaNacimiento;
+        this.contra=contra;
+        this.correo=correo;
+        this.genero=genero;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public String getfechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+}
